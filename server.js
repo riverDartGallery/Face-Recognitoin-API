b60589789ef4f58ee6c98f295acfea29720349f7
Jason Browning <jason@riverdartgallery.co.uk>
2019-03-19 19:12:50 +0000

    General Update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
 	ssl: true,
   }
 });

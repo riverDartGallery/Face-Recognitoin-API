8665e6c928332ad6ccc79c36f0556e732d4a8be1
Jason Browning <jason@riverdartgallery.co.uk>
2019-03-19 19:03:44 +0000

    General Update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-sinuous-94330',
     user : '',
     password : '',
     database : 'smart-brain'
@@ -24,7 +24,7 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => { res.send('it si working') })
+app.get('/', (req, res) => { res.send('it is working') })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
